88de4347aa61de58017b694a4b0668cdee1bdd2b
garybot <argot.pryer@gmail.com>
2019-09-24 23:33:52 -0700

    Updates

---
+++ precache-manifest.6eb35b44dd9ed0b65f665c1be9532f5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b0034e3c9009d5e84fdf6701881878",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "84272dce620668c9dc02",
+    "url": "/nasa-photo-of-the-day/static/css/main.f17e7332.chunk.css"
+  },
+  {
+    "revision": "7f5925b5768ac9b98292",
+    "url": "/nasa-photo-of-the-day/static/js/2.8e1dad3f.chunk.js"
+  },
+  {
+    "revision": "84272dce620668c9dc02",
+    "url": "/nasa-photo-of-the-day/static/js/main.bf63d03d.chunk.js"
+  },
+  {
+    "revision": "350a0b8fbf7e20aef48a",
+    "url": "/nasa-photo-of-the-day/static/js/runtime~main.03586219.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nasa-apod/precache-manifest.a0f00eb39c01d8360a569008a827d202.js"
+  "/nasa-photo-of-the-day/precache-manifest.6eb35b44dd9ed0b65f665c1be9532f5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-apod/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-photo-of-the-day/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
